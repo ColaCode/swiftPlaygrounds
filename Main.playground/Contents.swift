ee53dcb947b8569d30c20152591e78fe7ece027a
Vadym Poteriaiko <megabyte2004@gmail.com>
2023-03-17 21:34:42 +0100

    commit

---
+++ Main.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+
+//func squareDigits(_ num: Int) -> Int {
+//
+//var stringNum = Array(String(num))
+//var result = stringNum
+//    .map { $0.wholeNumberValue! * $0.wholeNumberValue! }
+//    .map { String($0) }
+//return Int(result.joined())!
+//
+//}
+//
+//squareDigits(21224)
